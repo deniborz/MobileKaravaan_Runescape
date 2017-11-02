9f294fa9d90148ec217a78e8060e2f01a70c7679
arnaudancis <arnaud.ancis@student.ucll.be>
2017-11-02 17:03:10 +0100

    lel

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,23 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Alert, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {text: 'Lel'};
+    }
+    _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        
+        <Text style={styles.title}>Welkom bij Karavaan</Text> 
+        <View style={styles.login}>  
+        <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.text}
+      />
+        </View>
       </View>
+
+      
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex: 1,
+      backgroundColor: 'lightblue',
+    
   },
+  title: {
+      fontSize: 35,
+      textAlign: 'center',
+      marginTop: 25,
+  },
+  login: {
+      padding: 15,
+      marginTop: 50,
+
+  },
+
 });
