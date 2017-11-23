135fdbed6a80fdae1e12cddfa9e17afc4255d077
arnaudancis <arnaud.ancis@student.ucll.be>
2017-11-23 17:10:58 +0100

    Login+navigation

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -2,37 +2,19 @@ import React from 'react';
 import { Button, Alert, StyleSheet, Text, View, TextInput } from 'react-native';
 
 import Login from './src/Components/Login/Login';
+import Vrienden from './src/Components/Vrienden/Vrienden';
+import {StackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {text: 'Lel'};
-    }
-    _onPressButton() {
-    Alert.alert('You tapped the button!')
-  }
+const Navigation = StackNavigator({
+    First: {screen: Login},
+    Second:{screen: Vrienden}, 
+})
+export default Navigation;
+
+/*class App extends React.Component {
   render() {
 ;    return (
       <Login />
     );
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      backgroundColor: 'lightblue',
-    
-  },
-  title: {
-      fontSize: 35,
-      textAlign: 'center',
-      marginTop: 25,
-  },
-  login: {
-      padding: 15,
-      marginTop: 50,
-
-  },
-
-});
+}*/
