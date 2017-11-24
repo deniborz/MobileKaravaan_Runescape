50110deec15f538d08d32c9c53d7ce02e8dceb67
Jens Wouters <jens_wouters@live.be>
2017-11-24 15:46:24 +0100

    merge

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-<<<<<<< HEAD
-import Login from  './src/Components/Login/Login';
-
-export default class App extends React.Component {
-=======
 import Login from './src/Components/Login/Login';
 import Vrienden from './src/Components/Vrienden/Vrienden';
 import {StackNavigator } from 'react-navigation';
@@ -17,23 +12,9 @@ const Navigation = StackNavigator({
 export default Navigation;
 
 /*class App extends React.Component {
->>>>>>> 135fdbed6a80fdae1e12cddfa9e17afc4255d077
   render() {
 ;    return (
       <Login />
     );
   }
-<<<<<<< HEAD
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-=======
 }*/
->>>>>>> 135fdbed6a80fdae1e12cddfa9e17afc4255d077
