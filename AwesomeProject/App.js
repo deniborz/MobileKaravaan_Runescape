23135cb665fb97a5e1be5036a612948889ed4eeb
Jens Wouters <jens_wouters@live.be>
2017-11-24 16:20:03 +0100

    wtf merge

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -6,15 +6,15 @@ import Vrienden from './src/Components/Vrienden/Vrienden';
 import {StackNavigator } from 'react-navigation';
 
 const Navigation = StackNavigator({
-    First: {screen: Login},
-    Second:{screen: Vrienden}, 
+    Login: {screen: Login},
+    Groep:{screen: Groepen}, 
 })
 export default Navigation;
 
 /*class App extends React.Component {
   render() {
 ;    return (
-      <Login />
+      <Login /> 
     );
   }
 }*/
